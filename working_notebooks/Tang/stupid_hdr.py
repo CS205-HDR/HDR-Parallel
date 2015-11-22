24e1a2be8f5ea6b3955c950e110747013f6debef
haosutang <haosutang@gmail.com>
2015-11-22 15:51:27 -0500

    Added stupid hdr..

---
+++ working_notebooks/Tang/stupid_hdr.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+im0 = Image.open("orig_0.jpg")
+im1 = Image.open("orig_1.jpg")
+im2 = Image.open("orig_2.jpg")
+im3 = Image.open("orig_3.jpg")
+
+id0 = np.array(im0.getdata())
+id1 = np.array(im1.getdata())
+id2 = np.array(im2.getdata())
+id3 = np.array(im3.getdata())
+
+id_comp = (id0+id1+id2+id3)/4
+
+id_comp2 = np.reshape(id_comp, (612,816,3)).astype(np.uint8)
+
+im_comp = Image.fromarray(id_comp2, 'RGB')
+
+im_comp.show()
